83c521e1185986f2f92a1a0c799e0c630a55e667
devalmathur5 <devalmathur5@gmail.com>
2022-12-02 17:00:04 +0530

    commit -2 for adding more code

--- Sample/src/Test3.java
+++ Sample/src/Test3.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 public class Test3 {
     public static void main(String[] args){
         String str = "hi hello hi";
@@ -24,10 +28,22 @@ public class Test3 {
         }
 
 
-        for(int i=0; i< token.length; i++){
-            System.out.println(token[i]);
-        }
+//        for(int i=0; i< token.length; i++){
+//            System.out.println(token[i]);
+//        }
 
         // code to find out the number of distinct substring
+        HashMap<String, Integer> hm = new HashMap<>();
+        for(int i=0; i<token.length; i++){
+            int temp=0;
+            for(int j=0; j<token.length; j++){
+                if(token[i].equalsIgnoreCase(token[j])){
+                    hm.put(token[i], ++temp);
+                }
+            }
+        }
+        for(Map.Entry<String, Integer> es : hm.entrySet()){
+            System.out.println(es.getKey() + "     " + es.getValue());
+        }
     }
 }
